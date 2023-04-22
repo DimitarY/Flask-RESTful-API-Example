84088f136fa6f7618c852f1ad68cbafca307afae
Dimitar Yordanov <dimitar.yordanov11@outlook.com>
2023-04-22 16:37:38 +0300

    Adding API versions
    Adding a way to use multiple version of the same API using Blueprint

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Api, Resource
 from flask_cors import CORS
 
-app = Flask(__name__)
-api = Api(app)
+app = Flask(__name__)  # Create a Flask application instance
+
+# Create a Blueprint for the API
+api_bp_v1 = Blueprint('apiV1', __name__, url_prefix='/v1')
+
 # Change it on release - CORS(app, resources={r"/*": {"origins": "http://example.com"}})
 CORS(app)
+
+# Creating API instance
+apiV1 = Api(api_bp_v1)
 
 
 class Home(Resource):
@@ -13,7 +19,10 @@ class Home(Resource):
         return {'message': 'Hello, World!'}, 200
 
 
-api.add_resource(Home, '/')
+apiV1.add_resource(Home, '/')
+
+# Register the Blueprint to the Flask application
+app.register_blueprint(api_bp_v1)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8393)
